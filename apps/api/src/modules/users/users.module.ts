e324c612dc103f7a2ba471616de2f2aee237b0aa
Imantha <umeshbandara08@gmail.com>
2026-08-08 09:51:31 +0530

    feat: import TenantModule into UsersModule to enable multi-tenancy support

--- apps/api/src/modules/users/users.module.ts
+++ apps/api/src/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SupabaseModule } from '../supabase/supabase.module';
+import { TenantModule } from '../tenant/tenant.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-    imports: [SupabaseModule],
+    imports: [SupabaseModule, TenantModule],
     controllers: [UsersController],
     providers: [UsersService],
     exports: [UsersService],
